5438b3d50240fe71e59271ac3a1da4d82bd2879a
lakithaomal <lhw150030@utdallas.edu>
2022-10-04 17:48:55 -0500

    grafanaUpdates

--- firmware/xu4Mqtt/grafanaUpdate.py
+++ firmware/xu4Mqtt/grafanaUpdate.py
@@ -59,13 +59,17 @@ def syncHostData(hostFound,hostID,hostIP):
         csvDataFiles = glob.glob(hostsDataFolder+"/"+hostID+ "/*/*/*/*.csv")
         print(csvDataFiles)
         for csvFile in csvDataFiles:
+            print()
             with open(csvFile, "r") as f:
+                print(csvFile)
+                print(csvFile.split("/"))
+                
                 reader = csv.DictReader(f)
                 rowList = list(reader)
-                for rowData in rowList:
-                    print("------------------------")
-                    print(rowData)
-                #     mL.writeMQTTLatestWearable(lk2,"OPCN3","nodeIDWearable")  
+                #for rowData in rowList:
+                #    print("------------------------")
+                    # print(rowData)
+                    # mL.writeMQTTLatestWearable(lk2,"OPCN3",hostID)  
 
 
 
