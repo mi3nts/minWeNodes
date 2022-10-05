0447f5526fbe267f8292eb6cadec3295bf121287
lakithaomal <lhw150030@utdallas.edu>
2022-10-05 14:01:45 -0500

    grafanaUpdates

--- firmware/xu4Mqtt/grafanaUpdate.py
+++ firmware/xu4Mqtt/grafanaUpdate.py
@@ -91,8 +91,8 @@ def gpsToggle(hostFound,hostIP):
             # print(out)
 
             time.sleep(0.1)
-            os.popen('scp ' + gpsOffJsonFile + ' teamlary@' +hostIP+":"+statusJsonFile).read()
-            print()
+            out = os.popen('scp ' + gpsOffJsonFile + ' teamlary@' +hostIP+":"+statusJsonFile).read()
+            #print()
             time.sleep(0.1)
           
             out = os.popen("ssh teamlary@"+ hostIP+' "cd /home/teamlary/gitHubRepos/minWeNodes/firmware/xu4Mqtt && nohup ./gpsReRun.sh >/dev/null 2>&1 &"').read()
@@ -104,8 +104,8 @@ def gpsToggle(hostFound,hostIP):
             # print(out)
    
             time.sleep(0.1)
-            os.popen('scp ' + gpsOnJsonFile + ' teamlary@' +hostIP+":"+statusJsonFile).read()
-            print()
+            out = os.popen('scp ' + gpsOnJsonFile + ' teamlary@' +hostIP+":"+statusJsonFile).read()
+            #print(out)
             time.sleep(0.1)
             
             out = os.popen("ssh teamlary@"+ hostIP+' "cd /home/teamlary/gitHubRepos/minWeNodes/firmware/xu4Mqtt &&  nohup ./gpsReRun.sh >/dev/null 2>&1 &"').read()
