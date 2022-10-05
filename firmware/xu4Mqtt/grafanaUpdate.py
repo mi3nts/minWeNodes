47aded653d1cb928fd73d3e78ef1e986be0d440f
lakithaomal <lhw150030@utdallas.edu>
2022-10-05 18:28:00 -0500

    grafanaUpdates

--- firmware/xu4Mqtt/grafanaUpdate.py
+++ firmware/xu4Mqtt/grafanaUpdate.py
@@ -102,7 +102,9 @@ def syncHostData(hostFound,hostID,hostIP):
 
                     latestDateTime    = readLatestTime(hostID,sensorID)
                     jsonDateTime = latestDateTime
+                    print(latestDateTime)
                     csvLatestDateTime = datetime.datetime.strptime(rowList[-1]['dateTime'],'%Y-%m-%d %H:%M:%S.%f')
+                    print(csvLatestDateTime)
                     if csvLatestDateTime > latestDateTime:
 
                         # for rowData in rowList:
