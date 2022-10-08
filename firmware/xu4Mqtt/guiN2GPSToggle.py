2c882ce1647eea5588fc47b873e9539b4d273e71
lakithaomal <lhw150030@utdallas.edu>
2022-10-08 18:28:26 -0500

    GUI Updates

--- firmware/xu4Mqtt/guiN2GPSToggle.py
+++ firmware/xu4Mqtt/guiN2GPSToggle.py
@@ -98,7 +98,7 @@ class wearableWindow(QMainWindow):
         self.statusBar = QtWidgets.QLabel(self)
         self.statusBar.setGeometry(QtCore.QRect(100,0,1000-100,50))
         self.statusBar.setText("MINTS Wearable EOD 001")
-        self.statusBar.setAlignment(QtCore.Qt.AlignCenter)
+        self.statusBar.setAlignment(QtCore.Qt.AlignLeft)
         self.statusBar.setStyleSheet("color: white;") 
         # self.statusBar.adjustSize()
         # self.statusBar.move(420,5)
