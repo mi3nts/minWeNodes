acb368854014c602a23babaaf5c22f0bb8213ca9
lakithaomal <lhw150030@utdallas.edu>
2022-10-08 21:03:17 -0500

    GUI Updates

--- firmware/xu4Mqtt/guiN2GPSToggle.py
+++ firmware/xu4Mqtt/guiN2GPSToggle.py
@@ -1,16 +1,17 @@
 # # Import tkinter and webview libraries
-from tkinter import *
-import webview
+# from tkinter import *
+# import webview
   
-# # define an instance of tkinter
-tk = Tk()
+# # # define an instance of tkinter
+# tk = Tk()
   
-# #  size of the window where we show our website
-tk.geometry("800x450")
+# # #  size of the window where we show our website
+# tk.geometry("800x450")
 
-# # Open website
-webview.create_window('MINTS', 'http://mdash.circ.utdallas.edu:3000/d/central_node_demo/central-node-demo?orgId=1&refresh=5s')
-webview.start()
+# # # Open website
+# webview.create_window('MINTS', 'http://mdash.circ.utdallas.edu:3000/d/central_node_demo/central-node-demo?orgId=1&refresh=5s')
+# webview.start()
+
 
 # Import tkinter and webview libraries
 import datetime
