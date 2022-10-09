1eea2d2e603635868680d9aa1bf6b9857d266866
lakithaomal <lhw150030@utdallas.edu>
2022-10-08 19:09:59 -0500

    GUI Updates

--- firmware/xu4Mqtt/guiN2GPSToggle.py
+++ firmware/xu4Mqtt/guiN2GPSToggle.py
@@ -101,7 +101,7 @@ class wearableWindow(QMainWindow):
         self.gpsButton = QtWidgets.QPushButton(self)
         self.gpsButton.setGeometry(QtCore.QRect(5,5,90,40))        
         self.gpsButton.setText("GPS")
-        self.gpsButton.setFont(QFont('SansSerif', 12, QFont.Bold))
+        self.gpsButton.setFont(QFont('SansSerif', 12))
         self.gpsButton.setStyleSheet("color: white;") 
         self.gpsButton.clicked.connect(self.mainGPS)
         
