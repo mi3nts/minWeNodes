c1b25a02fc853c697bda5798f1aaf523aed9d8f1
lakithaomal <lhw150030@utdallas.edu>
2022-10-14 05:50:55 -0500

    IP Reader Updates

--- firmware/xu4Mqtt/grafanaUpdate.py
+++ firmware/xu4Mqtt/grafanaUpdate.py
@@ -58,6 +58,7 @@ def getHostMac():
                     ("status"               ,1)
                     ])
                 mSR.sensorFinisherWearable(dateTime,hostID,"STATUS001",sensorDictionary)
+                time.sleep(10)
                 return True, hostID,hostIn['IP'];
             else:
                 print("Host " + hostID + " found with incorrect IP:" + ipAddress)
@@ -66,13 +67,14 @@ def getHostMac():
                     ("status"                , 0)
                     ])
                 mSR.sensorFinisherWearable(dateTime,hostID,"STATUS001",sensorDictionary)
-                
+                time.sleep(10)
                 # ADD Incorrect IP Error
                 sensorDictionary = OrderedDict([
                     ("dateTime"             ,str(dateTime)),
                     ("error"                , 5)
                     ])
-                mSR.sensorFinisherWearable(dateTime,hostID,"ERROR001",sensorDictionary)                
+                mSR.sensorFinisherWearable(dateTime,hostID,"ERROR001",sensorDictionary)  
+                time.sleep(10)              
                 return False, 0,0;
                     
         print("No hosts found")   
@@ -80,7 +82,8 @@ def getHostMac():
                         ("dateTime"             ,str(dateTime)),
                         ("status"               ,0)
                         ])
-        mSR.sensorFinisherWearable(dateTime,hostIn['nodeID'],"STATUS001",sensorDictionary)    
+        mSR.sensorFinisherWearable(dateTime,hostIn['nodeID'],"STATUS001",sensorDictionary)
+        time.sleep(10)  
     return False, -1,0;
 
 def readLatestTime(hostID,sensorID):
@@ -172,9 +175,8 @@ def syncHostData(hostFound,hostID,hostIP):
 def main():
     while True:
         hostFound,hostID,hostIP = getHostMac()
-        time.sleep(30)
         syncHostData(hostFound,hostID,hostIP)
-        time.sleep(30)
+        
 
 if __name__ == "__main__":
     print("=============")
